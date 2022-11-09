e1bbbd10faa6b08b41ea74efc6c4527a5e7d9755
DuckToFo <golden4gnvx@gmail.com>
2022-11-09 21:10:29 +0700

    added Exercise 1

--- js/exercise/ex1.js
+++ js/exercise/ex1.js
@@ -1,10 +1,27 @@
 console.log("Testing Node.")
-let a = prompt("Input start: ")
-let b = prompt("Input end: ")
-let i
-for (i = a; i <= b; i++) {
-    if (Number.isInteger(Math.sqrt(i)) == true) {
-        console.log("Valid square number: " + i)
+function findSquareNums() {
+    const a = document.getElementById("start").value
+    const b = document.getElementById("end").value
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            if (Number.isInteger(Math.sqrt(i)) == true) {
+                console.log("Valid square number: " + i)
+                let node = document.createElement("li")
+                let text = document.createTextNode("Valid square numbers: " + i)
+                node.appendChild(text)
+                document.getElementById("list").appendChild(node)
+            }
+        }
+    } else {
+        for (let i = a; i >= b; i--) {
+            if (Number.isInteger(Math.sqrt(i)) == true) {
+                console.log("Valid square number: " + i)
+                let node = document.createElement("li")
+                let text = document.createTextNode("Valid square numbers: " + i)
+                node.appendChild(text)
+                document.getElementById("list").appendChild(node)
+            }
+        }
     }
 }
 console.log("End.")
