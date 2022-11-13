95293aa78aed1ce87f4c7190d7601619a82a9479
DuckToFo <golden4gnvx@gmail.com>
2022-11-13 22:17:20 +0700

    minor modifications

--- js/exercise/disco.js
+++ js/exercise/disco.js
@@ -3,9 +3,10 @@ const setBg = () => {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = "#" + randomColor;
         color.innerHTML = "#" + randomColor; 
-    }, 100);
+    }, 300);
 }
 disco.addEventListener("click", setBg);
 function setWhite(){
-    document.body.style.backgroundColor = "#ffffff";
+    document.body.style.backgroundColor = "white";
 }
+stop.addEventListener("click", setWhite);
